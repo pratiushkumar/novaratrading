f30e1af89138dc96db7fcf9283422578225e3f71
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-05-15 23:52:02 +0530

    application.properties file configuration setting

---
+++ src/main/java/com/novara/NovaratradingApplication.java
@@ -0,0 +1,13 @@
+package com.novara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaratradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaratradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novara/NovaratradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaratradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
