cbf6cbd50c55878ac535b8cfa2d91f0be8f253e3
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-05-16 23:58:26 +0530

    created first home controller

---
+++ src/main/java/com/novara/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.novara.controller;
+
+
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class   HomeController {
+
+//    @GetMapping("")
+//    public ResponseEntity<ApiResponse> homeController(){
+//
+//
+//        ApiResponse res=new ApiResponse(
+//                "welcome to crypto treading platform working fine",
+//                true
+//        );
+//        return new ResponseEntity<ApiResponse>(res,HttpStatus.ACCEPTED);
+    @GetMapping
+    public String home() {
+        return "welcome to the novara trading platform ";
+    }
+    }
+
+
