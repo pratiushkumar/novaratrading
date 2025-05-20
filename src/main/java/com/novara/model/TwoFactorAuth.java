760a74cb857c8d689cd99b78c7595b978d87e923
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-05-20 23:08:02 +0530

    Two Factor authentication

---
+++ src/main/java/com/novara/model/TwoFactorAuth.java
@@ -0,0 +1,11 @@
+package com.novara.model;
+
+import com.novara.domain.VerificationType;
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+public class TwoFactorAuth {
+    private boolean isEnabled = false;
+    private VerificationType sendTo;
+}
